b51a0cf9c3ef39ccb0cc3fcde0cf562b6cffe9fc
Mauricio Portilla <lmauricioportillal@gmail.com>
2024-04-03 23:11:20 +0200

    Se desarrolla el componente no page found

--- Frontend/pruebasoft-web/src/app/app.module.ts
+++ Frontend/pruebasoft-web/src/app/app.module.ts
@@ -11,26 +11,28 @@ import { AuthModule } from './auth/auth.module';
 import { PagesModule } from './pages/pages.module';
 import { TokenInterceptor } from './interceptors/token.interceptor';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { SharedModule } from "./shared/shared.module";
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NoPageFoundComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    ToastrModule.forRoot(),
-    AppRoutingModule,
-    PagesModule,
-    AuthModule,
-    HttpClientModule
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NoPageFoundComponent
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    ],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot(),
+        AppRoutingModule,
+        PagesModule,
+        AuthModule,
+        HttpClientModule,
+        SharedModule
+    ]
 })
 export class AppModule { }
